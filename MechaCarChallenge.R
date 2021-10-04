f5a0a9490b5bd7e9ac7e3003ea203b3dc7a8f4ea
SheenaStroh <85318060+SheenaStroh@users.noreply.github.com>
2021-10-04 13:05:13 -0700

    uploading deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,12 @@
 library(dplyr)
 
-Mecha_mpg <- read.csv('MechaCar_mpg.csv') #import dataset
+Mecha_mpg <- read.csv('MechaCar_mpg.csv') #import dataset for deliverable 1
 
-reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_mpg)
-summary(reg)
+reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_mpg) #perform the multiple linear regression
+summary(reg) #view the summary of the regression
+
+coil <- read.csv('Suspension_Coil.csv') #import dataset for deliverable 2
+
+total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary<- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = "keep")
