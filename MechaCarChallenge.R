30ebdb714c81c8cb5fd1505df81066bbe95ceb10
SheenaStroh <85318060+SheenaStroh@users.noreply.github.com>
2021-10-04 13:34:26 -0700

    Added deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,6 +7,16 @@ summary(reg) #view the summary of the regression
 
 coil <- read.csv('Suspension_Coil.csv') #import dataset for deliverable 2
 
-total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #Create total summary dataframe
 
-lot_summary<- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = "keep")
+lot_summary<- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = "keep") #create lot summary dataframe
+
+t.test(coil$PSI, mu=1500) #T-Test across all manufacturing lots
+
+lot1 <- subset(coil,Manufacturing_Lot=="Lot1") #create subsets
+lot2 <- subset(coil,Manufacturing_Lot=='Lot2')
+lot3 <- subset(coil,Manufacturing_Lot=='Lot3')
+
+t.test(lot1$PSI, mu=1500) #t-test for each subset
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
